7ad0d399dd28d53f14d339d84b2821219f239425
Laycon Gomes <laycon.gomes@tld.com.br>
2025-01-22 15:24:49 -0300

    feat: implements TicketDAO on openTicket - TicketService

--- src/application/service/TicketService.ts
+++ src/application/service/TicketService.ts
@@ -1,15 +1,24 @@
 import crypto from 'crypto';
+import TicketDAO from '../dao/TicketDAO';
 // import { v4 as uuidv4 } from "uuid";
 
 export default class TicketService {
-    openTicket (requesterId: string, content: string) {
+
+    constructor(
+        readonly ticketDAO: TicketDAO
+    ){}
+
+    async openTicket (requesterId: string, content: string) {
         const ticketId = crypto.randomUUID()
+        //DTO: Data Transfer Object 
         const ticket = {
             ticketId,
             requesterId, 
             content,
+            startDate: new Date(),
             status: "open"
         }
+        await this.ticketDAO.save(ticket);
 
     }
 
