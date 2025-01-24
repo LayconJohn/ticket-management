426a24552ea7d9d25b1bf978fb45afd7ff55c300
Laycon Gomes <laycon.gomes@tld.com.br>
2025-01-24 17:57:30 -0300

    feat: use Queue on OpenTicket uscease

--- src/application/usecase/OpenTicket.ts
+++ src/application/usecase/OpenTicket.ts
@@ -1,15 +1,23 @@
 import Ticket from "../../domain/entity/Ticket"
+import Queue from "../../infra/queue/Queue";
 import TicketRepository from "../repository/TicketRepository";
 
 export default class OpenTicket {
 
     constructor(
-        readonly ticketRepository: TicketRepository
+        readonly ticketRepository: TicketRepository,
+        readonly queue: Queue
     ) {}
 
     async execute(input: Input): Promise<Output> {
         const ticket = Ticket.create(input.requesterId, input.content);
         await this.ticketRepository.save(ticket);
+
+        await this.queue.publish("ticketOpened", ticket);
+        // await this.queue.publish("processPayment", ticket);
+        // await this.queue.publish("integrateWithTrello", ticket);
+        // await this.queue.publish("sendEmail", ticket);
+
         return {
             ticketId: ticket.ticketId
         }
