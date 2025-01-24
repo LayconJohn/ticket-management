340a23ff3500f365e541406ce56144d6517e6ca0
Laycon Gomes <laycon.gomes@tld.com.br>
2025-01-24 17:18:09 -0300

    feat: create ImportTicket usecase

--- src/application/usecase/ImportTicket.ts
+++ src/application/usecase/ImportTicket.ts
@@ -1,5 +1,23 @@
+import Ticket from "../../domain/entity/Ticket";
+import TicketRepository from "../repository/TicketRepository";
+
 export default class ImporTicket {
-    execute() {
-        
+    
+    constructor(
+        readonly ticketRepository: TicketRepository
+    ) {}
+
+    async execute(input: Input): Promise<void> {
+        for(const ticketInput of input.tickets) {
+            const ticket = await Ticket.create(ticketInput.requesterId, ticketInput.content);
+            await this.ticketRepository.save(ticket);
+        }
     }
+}
+
+type Input = {
+    tickets: {
+        requesterId: string,
+        content: string
+    }[]
 }
