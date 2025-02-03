00de74d77120fe6e7e5699a1a0489b31a9abdafc
Laycon Gomes <laycon.gomes@tld.com.br>
2025-02-03 17:53:08 -0300

    feat: update create.sql

--- create.sql
+++ create.sql
@@ -1,4 +1,7 @@
-create john_user {
+drop schema john CASCADE;
+create schema john
+
+create john.user {
     user_id uuid primary key,
     email text,
     name text,
@@ -7,11 +10,14 @@ create john_user {
 
 create john.ticket {
     ticket_id uuid primary key,
-    requester_id uuid references john.user (user_id),
-    assignee_id uuid references john.user (user_id),
+    requester_id uuid not null references john.user (user_id),
+    assignee_id uuid not null references john.user (user_id),
     context text,
     status text,
     start_date timestamp,
     end_date timestamp,
     duration int
 }
+
+insert into john.user (user_id, email, name) values (1, 'John', '42e8bc9f-807b-4f70-9a8c-8fc69b44507e');
+insert into john.user (user_id, email, name) values (2, 'Laycon', '1d3b5fc5-94b0-4aaf-86ba-8fb01fd085b4');
